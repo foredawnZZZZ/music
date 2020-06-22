7a8b7571f19cb0f7576089c4d5648f2123da287a
foredawnZZZZ <18602151918@163.com>
2020-06-22 17:41:42 +0800

    06-22 singer ongoing

---
+++ src/common/listview/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <scroll class="listview" :data="data">
+    <ul>
+      <li v-for="(item,index) in data" :key="index" class="list-group">
+        <h1 class="list-group-title">{{item.title}}</h1>
+        <ul>
+          <li v-for="item in item.items" :key="item.id" class="list-group-item">
+            <img class="avatar" v-lazy="item.avatar" alt="">
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </scroll>
+</template>
+
+<script>
+import Scroll from "common/scroll";
+export default {
+  components: {
+    Scroll,
+  },
+  props: {
+    data: {
+      type: Array,
+      default: [],
+    },
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~assets/stylus/variable'
+.listview
+  position relative
+  width 100%
+  height 100%
+  overflow hidden
+  background $color-background
+  .list-group
+    padding-bottom 30px
+    .list-group-title
+      height 30px
+      line-height 30px
+      padding-left 20px
+      font-size $font-size-small
+      color $color-text-l
+      background $color-highlight-background
+    .list-group-item
+      display flex
+      align-items center
+      padding 20px 0 0 30px
+      .avatar
+        width 50px
+        height 50px
+        border-radius 50%
+      .name
+        margin-left 20px
+        color $color-text-l
+        font-size $font-size-medium
+</style>
+
--- src/views/singer/index.vue
+++ src/views/singer/index.vue
@@ -1,13 +1,83 @@
 <template>
-  <div></div>
+  <div class="singers">
+    <ListView :data="singerList"></ListView>
+  </div>
 </template>
 
 <script>
+import { getSingerList } from "api/singer";
+import ListView from "common/listview";
+import Singer from "assets/js/singer";
+const HOT_TITLE = "热门";
+const HOT_SINGER_LENGTH = 10;
 export default {
-  
-}
+  data() {
+    return {
+      singerList: [],
+    };
+  },
+  components: {
+    ListView,
+  },
+  mounted() {
+    this._getSingerList();
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then((res) => {
+        this.singerList = this._normalizeSingers(res.data.list);
+        console.log(this.singerList);
+      });
+    },
+    _normalizeSingers(list) {
+      let map = {
+        hot: {
+          title: HOT_TITLE,
+          items: [],
+        },
+      };
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LENGTH) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name,
+          }));
+        }
+        let key = item.Findex;
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: [],
+          };
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name,
+        }));
+      });
+      let abs = [];
+      let hots = [];
+      for (let k in map) {
+        let val = map[k];
+        if (/^[a-zA-Z]+$/.test(val.title)) {
+          abs.push(val);
+        } else if (val.title === HOT_TITLE) {
+          hots.push(val);
+        }
+      }
+      abs.sort((a, b) => {
+        return a.title.localeCompare(b.title);
+      });
+      return hots.concat(abs);
+    },
+  },
+};
 </script>
 
 <style lang="stylus" scoped>
-
+  .singers
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
